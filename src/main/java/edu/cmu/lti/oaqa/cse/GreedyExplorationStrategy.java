7794c0f1cd4fd399baa0a17d0184b495fd7f9332
Elmer Garduno <garduno@gmail.com>
2013-03-31 12:49:39 -0400

    Added impl to ExplorationStrategy

--- src/main/java/edu/cmu/lti/oaqa/cse/GreedyExplorationStrategy.java
+++ src/main/java/edu/cmu/lti/oaqa/cse/GreedyExplorationStrategy.java
@@ -1,5 +1,13 @@
 package edu.cmu.lti.oaqa.cse;
 
+import java.util.Iterator;
+
 public class GreedyExplorationStrategy implements ExplorationStrategy {
+
+  @Override
+  public Iterator getSpaceIterator() {
+    // TODO Auto-generated method stub
+    return null;
+  }
 
 }
